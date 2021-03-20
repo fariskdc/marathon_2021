6515fb9cefe82b0c5d5d67c7236277709ab7d459
hllvc <hllvc@icloud.com>
2021-03-20 21:48:32 +0100

    merge commit

--- app/src/components/HomePageStudent.js
+++ app/src/components/HomePageStudent.js
@@ -1,38 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const mongoose = require("mongoose");
 
 const HomePageStudent = () => {
+  const [student, setStudent] = useState({
+    id: "",
+    name: "",
+    surname: "",
+    semester: 0,
+    email: "",
+    phone: "",
+  });
 
-    const [quote, setQuote] = useState('');
-    const [author, setAuthor] = useState('');
-    const [loading, setLoading] = useState(true);
-    
-    useEffect(()=>{
-        axios.get('https://type.fit/api/quotes')
-        .then(res=>{
-            
-            console.log(res.data)
-            setLoading(false);
-            setQuote(res.data[17].text);
-            setAuthor(res.data[17].author);
-        })
-        .catch(err=>{
-            setLoading(true);
-            console.log(err);
-        });
-    })
+  useEffect(() => {
+    axios.get(
+      "mongodb+srv://hllvc:hllvc@marathon.meoev.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      (req, res) => {}
+    );
+  });
 
-    return (
-        <div>
-            <h1> Dobro došao ime </h1>
-            {loading ? 'Loading....' : <h2> {quote} by {author}</h2>}
-            
-        </div>
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    axios
+      .get("https://type.fit/api/quotes")
+      .then((res) => {
+        console.log(res.data);
+        setLoading(false);
+        setQuote(res.data[17].text);
+        setAuthor(res.data[17].author);
+      })
+      .catch((err) => {
+        setLoading(true);
+        console.log(err);
+      });
+  });
 
-    )
-
-}
+  return (
+    <div>
+      <h1> Dobro došao ime </h1>
+      {loading ? (
+        "Loading...."
+      ) : (
+        <h2>
+          {" "}
+          {quote} by {author}
+        </h2>
+      )}
+    </div>
+  );
+};
 
 export default HomePageStudent;
