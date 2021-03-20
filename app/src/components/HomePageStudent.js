3f37c3b101a82f1f0a88795c3d6e2a2ad804d0f1
omc-e <76270236+omc-e@users.noreply.github.com>
2021-03-20 22:55:44 +0100

    export to pdf

--- app/src/components/HomePageStudent.js
+++ app/src/components/HomePageStudent.js
@@ -1,23 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-const mongoose = require("mongoose");
+
 
 const HomePageStudent = () => {
-  const [student, setStudent] = useState({
-    id: "",
-    name: "",
-    surname: "",
-    semester: 0,
-    email: "",
-    phone: "",
-  });
+  
 
-  useEffect(() => {
-    axios.get(
-      "mongodb+srv://hllvc:hllvc@marathon.meoev.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-      (req, res) => {}
-    );
-  });
+  
 
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
---
+++ app/src/components/PrintGrades.js
@@ -0,0 +1,36 @@
+import React, { useState, PureComponent } from 'react';
+import jsPDF from 'jspdf'; 
+
+const PrintGrades = () => {
+
+    const jsPDFGenerate = () => {
+
+        const student = {
+            name : 'Emsar',
+            surname : 'Omic',
+            jmbg : '1709000180087',
+            prosjek : '8.2',
+            number : '387644402963'
+        }; 
+    
+        
+    
+        var doc = new jsPDF ('p' , 'pt');
+    
+        doc.text(30,30, student.name);
+        
+        doc.save ('generated.pdf');
+    
+    }
+
+    return (
+
+        <button onClick = {jsPDFGenerate}>
+            Printaj Prosjek
+        </button>
+
+    )
+
+}
+
+export default PrintGrades;
