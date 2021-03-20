a6fd9d7857f31922c916653e63b21030ee2dedfc
hllvc <hllvc@icloud.com>
2021-03-20 23:05:05 +0100

    fixing merge conflict and errors

--- app/src/components/HomePageStudent.js
+++ app/src/components/HomePageStudent.js
@@ -2,6 +2,14 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const HomePageStudent = () => {
+  const [student, setStudent] = useState({
+    id: "",
+    name: "",
+    surname: "",
+    semester: 0,
+    email: "",
+    phone: "",
+  });
   useEffect(() => {
     axios({
       method: "GET",
@@ -9,16 +17,18 @@ const HomePageStudent = () => {
       headers: {
         "Content-Type": "application/json",
       },
-    }).then((res) => {
-      setStudent({
-        id: res.data.id,
-        name: res.data.name,
-        surname: res.data.surname,
-        semester: res.data.semester,
-        email: res.data.email,
-        phone: res.data.phone,
-      });
-    });
+    })
+      .then((res) => {
+        setStudent({
+          id: res.data.id,
+          name: res.data.name,
+          surname: res.data.surname,
+          semester: res.data.semester,
+          email: res.data.email,
+          phone: res.data.phone,
+        });
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   const [quote, setQuote] = useState("");
