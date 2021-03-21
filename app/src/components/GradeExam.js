167bf5d45ff7a8652fae7694a130ea1b7dd04f1f
omc-e <76270236+omc-e@users.noreply.github.com>
2021-03-21 11:50:47 +0100

    bug fixes

--- app/src/components/GradeExam.js
+++ app/src/components/GradeExam.js
@@ -12,10 +12,6 @@ function GradeExam(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setGrade({
-      ...grade,
-      from: `${professor.name} ${professor.surname}`,
-    });
   };
 
   return (
@@ -25,22 +21,19 @@ function GradeExam(props) {
         <input
           name="name"
           type="text"
-          value={grade.name}
-          onChange={(e) => setGrade(...grade, (grade.name = e.target.value))}
+          
         />
         <label htmlFor="student">Broj indexa studenta</label>
         <input
           name="student"
           type="text"
-          value={grade.student}
-          onChange={(e) => setGrade(...grade, (grade.student = e.target.value))}
+          
         />
         <label htmlFor="grade">Ocjena</label>
         <input
           name="grade"
           type="text"
-          value={grade.grade}
-          onChange={(e) => setGrade(...grade, (grade.grade = e.target.value))}
+          
         />
         <button type="submit">Ocijeni</button>
       </form>
