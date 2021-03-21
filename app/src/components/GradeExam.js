8619ce6c254494691121840328643780f9ebaa4e
hllvc <hllvc@icloud.com>
2021-03-21 11:52:48 +0100

    bug fix

--- app/src/components/GradeExam.js
+++ app/src/components/GradeExam.js
@@ -1,51 +1,41 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 function GradeExam(props) {
-  const { professor } = props;
+	const {professor} = props;
 
-  const [grade, setGrade] = useState({
-    name: "",
-    from: "",
-    student: "",
-    grade: "",
-  });
+	const [grade, setGrade] = useState({
+		name: "",
+		from: "",
+		student: "",
+		grade: "",
+	});
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setGrade({
-      ...grade,
-      from: `${professor.name} ${professor.surname}`,
-    });
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
 
-  return (
-    <div className="grade-exam">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Ime predmeta</label>
-        <input
-          name="name"
-          type="text"
-          value={grade.name}
-          onChange={(e) => setGrade(...grade, (grade.name = e.target.value))}
-        />
-        <label htmlFor="student">Broj indexa studenta</label>
-        <input
-          name="student"
-          type="text"
-          value={grade.student}
-          onChange={(e) => setGrade(...grade, (grade.student = e.target.value))}
-        />
-        <label htmlFor="grade">Ocjena</label>
-        <input
-          name="grade"
-          type="text"
-          value={grade.grade}
-          onChange={(e) => setGrade(...grade, (grade.grade = e.target.value))}
-        />
-        <button type="submit">Ocijeni</button>
-      </form>
-    </div>
-  );
+	return (
+		<div className="grade-exam">
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="name">Ime predmeta</label>
+				<input
+					name="name"
+					type="text"
+				/>
+				<label htmlFor="student">Broj indexa studenta</label>
+				<input
+					name="student"
+					type="text"
+				/>
+				<label htmlFor="grade">Ocjena</label>
+				<input
+					name="grade"
+					type="text"
+				/>
+				<button type="submit">Ocijeni</button>
+			</form>
+		</div>
+	);
 }
 
 export default GradeExam;
