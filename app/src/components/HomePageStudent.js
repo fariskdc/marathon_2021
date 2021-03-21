ec27a114cda49b6a12df00a9d5e795efbc902a0d
hllvc <hllvc@icloud.com>
2021-03-21 04:43:22 +0100

    redesign of components

--- app/src/components/HomePageStudent.js
+++ app/src/components/HomePageStudent.js
@@ -1,40 +1,16 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import NavBar from "./NavBar";
+import Index from "./Index";
+import Schedule from "./Schedule";
+import WelcomePage from "./WelcomePage";
 
 const HomePageStudent = (props) => {
   const { student } = props;
-  const [quote, setQuote] = useState("");
-  const [author, setAuthor] = useState("");
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    axios
-      .get("https://type.fit/api/quotes")
-      .then((res) => {
-        setLoading(false);
-        setQuote(res.data[17].text);
-        setAuthor(res.data[17].author);
-      })
-      .catch((err) => {
-        setLoading(true);
-        console.log(err);
-      });
-    console.log(props);
-  }, []);
 
   return (
     <div>
-      <NavBar />
-      <h1> Dobro došao {student.name} </h1>
-      {loading ? (
-        "Loading...."
-      ) : (
-        <h2>
-          {" "}
-          {quote} by {author}
-        </h2>
-      )}
+      <WelcomePage student={student} />
+      <Schedule />
+      <Index student={student} />
     </div>
   );
 };
---
+++ app/src/components/WelcomePage.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function WelcomePage(props) {
+  const { student } = props;
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get("https://type.fit/api/quotes")
+      .then((res) => {
+        setLoading(false);
+        setQuote(res.data[17].text);
+        setAuthor(res.data[17].author);
+      })
+      .catch((err) => {
+        setLoading(true);
+        console.log(err);
+      });
+    console.log(props);
+  }, []);
+
+  return (
+    <div>
+      <h1> Dobro došao {student.name} </h1>
+      {loading ? (
+        "Loading...."
+      ) : (
+        <h2>
+          {" "}
+          {quote} by {author}
+        </h2>
+      )}
+    </div>
+  );
+}
+
+export default WelcomePage;
